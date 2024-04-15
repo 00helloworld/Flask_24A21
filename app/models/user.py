c8e17bfbbf999f65c6020c5056aea059782f3923
yanshen <yanshen0406@gmail.com>
2024-04-15 01:33:19 -0500

    test statistic and user table relation

--- app/models/user.py
+++ app/models/user.py
@@ -22,6 +22,7 @@ class User(db.Model):
 class UserAttempt(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer,  db.ForeignKey('user.id'), nullable=False)
+    user_name = db.Column(db.Integer,  db.ForeignKey('user.username'), nullable=False)
     assessment_id = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     score = db.Column(db.Float, nullable=False)
